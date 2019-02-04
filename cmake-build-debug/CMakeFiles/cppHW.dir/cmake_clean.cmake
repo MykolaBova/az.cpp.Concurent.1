9de95d9dfc0901a64ce35d19eb427174bc754b23
mykolabova <Mykola.Bova@innovecs.com>
2019-02-04 19:48:27 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/cppHW.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppHW.dir/main.cpp.o"
+  "cppHW.pdb"
+  "cppHW"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppHW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
