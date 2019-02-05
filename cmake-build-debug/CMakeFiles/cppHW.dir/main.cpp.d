e7aea263fd059425ac84a3657ca678809a02ba84
mykolabova <Mykola.Bova@innovecs.com>
2019-02-05 13:08:35 +0200

    - tested threads example - added Lippman code - tested Lippman code

---
+++ cmake-build-debug/CMakeFiles/cppHW.dir/main.cpp.d
@@ -0,0 +1,5 @@
+CMakeFiles/cppHW.dir/main.cpp.o: \
+ /home/mykolabova/_bova/code/az.cpp.Concurent.1/main.cpp \
+ /home/mykolabova/_bova/code/az.cpp.Concurent.1/Sales_item.h
+
+/home/mykolabova/_bova/code/az.cpp.Concurent.1/Sales_item.h:
